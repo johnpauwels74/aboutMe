43a7e8d2009643b30ff84701661ac8744cbdb5d0
johnpauwels74 <johnpauwels74@gmail.com>
2017-08-29 19:30:34 -0500

    adding another line

--- src/com/aboutMe/java/Main.java
+++ src/com/aboutMe/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// using println
+        //this is me making changes
         System.out.println("Hi! My name is John!");
         System.out.println("I like to program. I have been programming since I started college in 2003.");
         System.out.println("I have learned quite a few programming languages since then.");
