7c89cc41e18c6e74c4529d85935a72fb4fcc5f8a
johnpauwels74 <johnpauwels74@gmail.com>
2017-08-29 19:19:47 -0500

    Initial commit

---
+++ src/com/aboutMe/java/Main.java
@@ -0,0 +1,12 @@
+package com.aboutMe.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// using println
+        System.out.println("Hi! My name is John!");
+        System.out.println("I like to program. I have been programming since I was nine.");
+        System.out.println("I have been working with Java since the late 1990's.");
+        System.out.println("It has changed a lot in that time. ");
+    }
+}
